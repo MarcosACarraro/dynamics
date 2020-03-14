6363900b2cccd814c547922642f35493b91f3024
Marcos Carraro <marcoscarraro@hotmail.com>
2020-03-13 22:51:43 -0300

    Plugin

---
+++ Plugin/AccountPosOperation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Plugin
+{
+    public class AccountPosOperation : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (context.MessageName.ToLower() == "update" &&
+                context.Mode == Convert.ToInt32(MeuEnum.Mode.Synchronous) &&
+                context.Stage == Convert.ToInt32(MeuEnum.Stage.PosOperation))
+            {
+                IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+                var serviceAdmin = factory.CreateOrganizationService(null);
+
+                ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+                trace.Trace("Inicio Plugin");
+                Entity entidadeContexto = null;
+                Entity entidadePre = null;
+
+                if (context.InputParameters.Contains("Target"))
+                    entidadeContexto = (Entity)context.InputParameters["Target"];
+
+                if (context.PreEntityImages.Contains("preImagen"))
+                    entidadePre = (Entity)context.PreEntityImages["preImagen"];
+
+                if (entidadeContexto == null || entidadePre == null)
+                    return;
+
+                if ((entidadeContexto.Contains("primarycontactid") && entidadePre.Contains("primarycontactid")) &&
+                    (entidadeContexto["primarycontactid"]) != (entidadePre["primarycontactid"]))
+                    throw new InvalidPluginExecutionException("Não é possivel altear o contato primário");
+            }
+        }
+    }
+}
---
+++ Plugin/AccountPreValidation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Plugin
+{
+    public class AccountPreValidation : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+            ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            Entity entidadeConexto = null;
+
+            if (context.InputParameters.Contains("Target"))
+                entidadeConexto = (Entity)context.InputParameters["Target"];
+
+            if (entidadeConexto == null)
+                return;
+
+            if (!entidadeConexto.Contains("primarycontactid"))
+                throw new InvalidPluginExecutionException(OperationStatus.Failed, "Contato relacionado obrigatório!!");
+
+        }
+    }
+}
